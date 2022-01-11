e06da2290ff4a82f5b1e1e93322e7429485be0ca
Rashika1998 <rashikamr1998@gmail.com>
2022-01-11 11:59:22 +0530

    Consumer implementation.

---
+++ src/main/java/functionalInterface/_Consumer.java
@@ -0,0 +1,40 @@
+package functionalInterface;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+
+        //Imperative approach
+        greedCustomer(new Customer("Rashika", "+94768003266"));
+
+        //Declarative approach
+        Customer customerConsumer = new Customer("Sandushi", "+94710000977");
+        greedCustomerConsumer.accept(customerConsumer);
+
+    }
+
+    //Imperative approach
+    static void greedCustomer(Customer customer){
+        System.out.println("Hello " + customer.customerName +
+                ", thank for registering phone " +
+                customer.customerPhoneNumber);
+    }
+
+    //Declarative approach
+    static Consumer<Customer> greedCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thank for registering phone number " +
+                    customer.customerPhoneNumber);
+
+    static class Customer{
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
